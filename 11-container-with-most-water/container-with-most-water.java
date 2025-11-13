bf9df04767521223940f93fee7bc313e66dc77ad
Chowdary Manikanta Yarramaneni <manikantayarramaneni.1998@gmail.com>
2025-11-13 18:06:05 -0500

    Time: 3 ms (96.32%) | Memory: 77.3 MB (7.28%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+        int max=0;
+        while(i<j)
+        {
+            if(height[i]<=height[j]){
+                max=Math.max(max,height[i]*(j-i));
+                i++;
+            }
+            else{
+                max=Math.max(max,height[j]*(j-i));
+                j--;
+            }
+        }
+        return max;
+    }
+}
